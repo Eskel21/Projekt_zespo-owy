4ca59b73706265518cab2a8e2a29c40cace590d4
Bartłomiej Wojcik <github.bartek.wojcik@gmail.com>
2023-11-08 00:15:50 +0100

    Note update

--- Models/Note.cs
+++ Models/Note.cs
@@ -5,6 +5,5 @@ public class Note
     public int Id_note { get; set; }
     public string Title { get; set; }
     public string Descritpion { get; set; }
-    public int Id_user { get; set; }
     public string Content { get; set; }
 }
