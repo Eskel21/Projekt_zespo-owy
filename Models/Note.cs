a041932a6297bcc40e022834e740d2c038e90dc8
Bartłomiej Wojcik <github.bartek.wojcik@gmail.com>
2023-11-07 23:50:16 +0100

    Ok

--- Models/Note.cs
+++ Models/Note.cs
@@ -2,12 +2,9 @@
 
 public class Note
 {
-    public Note()
-    {
-        public int Id_note { get; set; }
+    public int Id_note { get; set; }
     public string Title { get; set; }
     public string Descritpion { get; set; }
     public int Id_user { get; set; }
     public string Content { get; set; }
-    }
 }
